12e445413c625ebc5932257d6965186a60fb97bd
Sagar Deshpande <deshpandesagar1704@gmail.com>
2015-10-06 16:08:00 -0500

    Added REST Apis for getting the data from Mongo DB using mongoose

--- app/api.js
+++ app/api.js
@@ -2,7 +2,11 @@ var express = require('express');
 var router  = express.Router();
 
 //Models
-var Room_Indicator         = require('./models/room_indicator');
+var Place          = require('./models/place');
+var Building       = require('./models/building');
+var Level          = require('./models/level');
+var Room_Indicator = require('./models/room_indicator');
+var Room           = require('./models/room');
 
 // middleware to use for all requests
 router.use(function(req, res, next) {
@@ -18,13 +22,136 @@ router.get('/', function(req, res) {
     res.json({ message: 'hooray! welcome to our api!' });
 });
 
-//Get the room indicators
+//Get the place - On a high level like - 
+// An University
+// A mall etc.
 router.post('/getPlace', 
 	function(req, res) {
 		console.log("In getPlace API");
+		var body = req.body;
+		console.log("Place:: " + body.place);
+		
 		var getPlaceQuery = {
-			place_name : "The University of Texas at Dallas"
+			place_name : body.place
 		};
+
+		console.log(getPlaceQuery);
+
+		var columns = {
+			_id : 1
+		}
+		
+		//Get the place id
+		Place.find(getPlaceQuery, columns, function(err, places) {
+			if (err) {
+                console.log(err);
+                res.send(err);
+            }
+            console.log(places);
+
+            res.json(
+            {
+            	status : 200,
+            	place  : places
+            });
+
+		});
 	}
 );
+
+//Get the building in a particular place
+router.post('/getBuilding', 
+	function(req, res) {
+		console.log("In getBuilding API");
+		var body = req.body;
+		console.log("Building:: " + body.building);
+		
+		var getBuildingQuery = {
+			place_id      : body.place_id,
+			building_name : body.building
+		};
+
+		var columns = {
+			_id : 1
+		};
+		
+		//Get the building id
+		Building.find(getBuildingQuery, columns, function(err, buildings) {
+			if (err) {
+                console.log(err);
+                res.send(err);
+            }
+            console.log(buildings);
+
+            res.json(
+            {
+            	status   : 200,
+            	building : buildings
+            });
+
+		});
+	}
+);
+
+//Get the level number in a building
+router.post('/getLevel', 
+	function(req, res) {
+		console.log("In getLevel API");
+		var body = req.body;
+		console.log("Level:: " + body.level);
+		
+		var getLevelQuery = {
+			building_id  : body.building_id,
+			level_number : body.level
+		};
+		
+		//Get the building id
+		Level.find(getLevelQuery, function(err, levels) {
+			if (err) {
+                console.log(err);
+                res.send(err);
+            }
+            console.log(levels);
+
+            res.json(
+            {
+            	status : 200,
+            	level  : levels
+            });
+
+		});
+	}
+);
+
+//Get the room indicators
+router.post('/getRoomIndicator', 
+	function(req, res) {
+		console.log("In getRoomIndicator API");
+		var body = req.body;
+		console.log("RoomIndicator:: " + body.indicator);
+		
+		var getRoomIndicatorQuery = {
+			level_id  : body.level_id,
+			indicator : body.indicator
+		};
+		
+		//Get the room indicator id
+		Room_Indicator.find(getRoomIndicatorQuery, function(err, indicators) {
+			if (err) {
+                console.log(err);
+                res.send(err);
+            }
+            console.log(indicators);
+
+            res.json(
+            {
+            	status    : 200,
+            	indicator : indicators
+            });
+
+		});
+	}
+);
+
+
 module.exports = router;
--- app/models/place.js
+++ app/models/place.js
@@ -1,9 +1,10 @@
 var mongoose = require('mongoose');
+mongoose.set('debug', true);
 
 // define the schema for our user model
 var placeSchema = new mongoose.Schema({
 	_id         : String,
-	place_name : String
+	place_name  : String
 });
 
 // methods ======================
