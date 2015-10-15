42e9ee93a21f4cc693be10c40f8898f9a99d335f
Sagar Deshpande <deshpandesagar1704@gmail.com>
2015-10-15 12:00:36 -0500

    Added the update APIs

--- app/api.js
+++ app/api.js
@@ -8,6 +8,8 @@ var Level          = require('./models/level');
 var Room_Indicator = require('./models/room_indicator');
 var Room           = require('./models/room');
 
+var upsert = {upsert : true};
+
 // middleware to use for all requests
 router.use(function(req, res, next) {
     // do logging
@@ -105,7 +107,7 @@ router.post('/getLevel',
 			level_number : body.level
 		};
 		
-		//Get the building id
+		//Get the level id
 		Level.find(getLevelQuery, function(err, levels) {
 			if (err) {
                 console.log(err);
@@ -153,5 +155,236 @@ router.post('/getRoomIndicator',
 	}
 );
 
+//Get the rooms
+router.post('/getRoom', 
+	function(req, res) {
+		console.log("In getRoom API");
+		var body = req.body;
+		console.log("Room:: " + body.room_name);
+		
+		var getRoomQuery = {
+			indicator_id : body.indicator_id,
+			room_name    : body.room_name
+		};
+		
+		//Get the room indicator id
+		Room.find(getRoomQuery, function(err, rooms) {
+			if (err) {
+                console.log(err);
+                res.send(err);
+            }
+            console.log(rooms);
+
+            res.json(
+            {
+            	status : 200,
+            	room   : rooms
+            });
+
+		});
+	}
+);
+
+//Update places
+router.post('/updatePlace', 
+	function(req, res) {
+		console.log("In updatePlace API");
+		var body = req.body;
+		console.log("Place:: " + body.place + ", " + body.new_place);
+		
+		var getPlaceQuery = {
+			place_name : body.place
+		};
+
+		var setPlaceQuery = {
+			$set : {
+				place_name : body.new_place
+			}
+		};
+
+		console.log(getPlaceQuery);
+		
+		//Update the place name
+		Place.update(getPlaceQuery, setPlaceQuery, upsert, function(err, places) {
+			if (err) {
+                console.log(err);
+                res.send(err);
+            }
+            console.log(places);
+
+            res.json(
+            {
+            	status : 200,
+            	place  : places
+            });
+
+		});
+	}
+);
+
+//Update the building in a particular place
+router.post('/updateBuilding', 
+	function(req, res) {
+		console.log("In updateBuilding API");
+		var body = req.body;
+		console.log("Building:: " + body.building + ", " + body.new_building);
+		
+		var getBuildingQuery = {
+			place_id      : body.place_id,
+			building_name : body.building
+		};
+
+		var setBuildingQuery = {
+			$set : {
+				place_id      : body.place_id,
+				building_name : body.new_building
+			}
+		};
+		
+		//Update building name
+		Building.update(getBuildingQuery, setBuildingQuery, upsert, function(err, buildings) {
+			if (err) {
+                console.log(err);
+                res.send(err);
+            }
+            console.log(buildings);
+
+            res.json(
+            {
+            	status   : 200,
+            	building : buildings
+            });
+
+		});
+	}
+);
+
+//Update the level number in a building
+router.post('/updateLevel', 
+	function(req, res) {
+		console.log("In updateLevel API");
+		var body = req.body;
+		console.log("Level:: " + body.level + ", " + body.new_level);
+		
+		var getLevelQuery = {
+			building_id  : body.building_id,
+			level_number : body.level
+		};
+
+		var setLevelQuery = {
+			$set : {
+				building_id  : body.building_id,
+				level_number : body.new_level
+			}
+		};		
+		
+		//Update Building Level
+		Level.update(getLevelQuery, setLevelQuery, upsert, function(err, levels) {
+			if (err) {
+                console.log(err);
+                res.send(err);
+            }
+            console.log(levels);
+
+            res.json(
+            {
+            	status : 200,
+            	level  : levels
+            });
+
+		});
+	}
+);
+
+//Update the room indicators
+router.post('/updateRoomIndicator', 
+	function(req, res) {
+		console.log("In updateRoomIndicator API");
+		var body = req.body;
+		console.log("RoomIndicator:: " + body.indicator + ", " + body.new_indicator);
+		
+		var getRoomIndicatorQuery = {
+			level_id  : body.level_id,
+			indicator : body.indicator
+		};
+		
+		var setRoomIndicatorQuery = {
+			$set : {
+				level_id  : body.level_id,
+				indicator : body.new_indicator,
+				has_room  : body.new_has_room,
+				nearest_north : {
+					north_id : body.new_north_id,
+					distance : body.new_north_distance
+				},
+				nearest_east : {
+					east_id  : body.new_east_id,
+					distance : body.new_east_distance
+				},
+				nearest_south : {
+					south_id : body.new_south_id,
+					distance : body.new_south_distance
+				},
+				nearest_west : {
+					west_id  : body.new_west_id,
+					distance : body.new_west_distance
+				}
+			}
+		};
+
+		//Update the room indicator
+		Room_Indicator.update(getRoomIndicatorQuery, setRoomIndicatorQuery, upsert, function(err, indicators) {
+			if (err) {
+                console.log(err);
+                res.send(err);
+            }
+            console.log(indicators);
+
+            res.json(
+            {
+            	status    : 200,
+            	indicator : indicators
+            });
+
+		});
+	}
+);
+
+//Update rooms
+router.post('/updateRoom', 
+	function(req, res) {
+		console.log("In updateRoom API");
+		var body = req.body;
+		console.log("Room:: " + body.room_name + ", " + body.new_room_name);
+		
+		var getRoomQuery = {
+			indicator_id : body.indicator_id,
+			room_name    : body.room_name
+		};
+		
+		var setRoomQuery = {
+			$set : {
+				indicator_id : body.indicator_id,
+				room_name    : body.new_room_name
+			}
+		}
+
+		//Update the room
+		Room.update(getRoomQuery, setRoomQuery, upsert, function(err, rooms) {
+			if (err) {
+                console.log(err);
+                res.send(err);
+            }
+            console.log(rooms);
+
+            res.json(
+            {
+            	status : 200,
+            	room   : rooms
+            });
+
+		});
+	}
+);
 
 module.exports = router;
